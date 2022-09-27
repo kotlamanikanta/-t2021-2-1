93dc174d07246de3136a6e7af91e802f913eb117
kotlamanikanta <114420784+kotlamanikanta@users.noreply.github.com>
2022-09-27 14:13:47 +0530

    Add files via upload

---
+++ Program-3.py
@@ -0,0 +1,13 @@
+n= int(input("a="))
+i=1
+if(n%2==0):
+    n=n-1
+    while(n>0):
+        print(i)
+        i=i+2
+        n=n-1
+else:
+    while(n>0):
+        print(i)
+        i=i+2
+        n=n-1 
